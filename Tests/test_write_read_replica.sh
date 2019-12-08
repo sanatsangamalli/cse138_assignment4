d68dc942e2bb9f30f3f08bba8a0fcf5b40a05d96
sanatsangamalli <ssangama@ucsc.edu>
2019-12-07 23:53:13 -0800

    final commit

--- Tests/test_write_read_replica.sh
+++ Tests/test_write_read_replica.sh
@@ -4,7 +4,8 @@
 set -x
 
 # write sampleKey1 = value1 to replica1
-./Tests/put.sh 127.0.0.1:13805 "sampleKey1" "value10" "{}"
+#./Tests/put.sh 127.0.0.1:13802 "sampleKey1" "value10" "{}"
 # read sampleKey1 from replica2 to make sure it's correct
-./Tests/get.sh 127.0.0.1:13803 "sampleKey1" "{\"10.10.0.2:13800\":5,\"10.10.0.3:13800\":0,\"10.10.0.4:13800\":0}"
-
+#./Tests/get.sh 127.0.0.1:13802 "sampleKey1" "{\"10.10.0.2:13800\":1,\"10.10.0.3:13800\":0,\"10.10.0.4:13800\":0}"
+#./Tests/put.sh 127.0.0.1:13803 "sampleKey1" "value5" "{\"10.10.0.2:13800\":2,\"10.10.0.3:13800\":0,\"10.10.0.4:13800\":0}"
+./Tests/get.sh 127.0.0.1:13802 "sampleKey1" "{\"10.10.0.2:13800\":2,\"10.10.0.3:13800\":1,\"10.10.0.4:13800\":0}"
