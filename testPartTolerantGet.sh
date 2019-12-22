76cc15c9ed9137176c1c21b62c611f5ecafd961b
sanatsangamalli <ssangama@ucsc.edu>
2019-12-21 18:03:40 -0800

    add test file

--- testPartTolerantGet.sh
+++ testPartTolerantGet.sh
@@ -1,8 +1,9 @@
 ./Tests/put.sh localhost:13802 sampleKey sampleValue {}
-./Tests/put.sh localhost:13803 sampleKey sampleValue {}
-./Tests/get.sh localhost:13804 sampleKey
-./Tests/get.sh localhost:13805 sampleKey
- docker network disconnect kv_subnet node1
-./Tests/get.sh localhost:13804 sampleKey
-docker network disconnect kv_subnet node2
- ./Tests/get.sh localhost:13804 sampleKey
+#./Tests/put.sh localhost:13803 sampleKey sampleValue {}
+#./Tests/get.sh localhost:13804 sampleKey
+./Tests/get.sh localhost:13805 sampleKey {}
+ docker network disconnect kv_subnet node4
+./Tests/get.sh localhost:13804 sampleKey {}
+#docker network disconnect kv_subnet node2
+ #./Tests/get.sh localhost:13804 sampleKey
+./Tests/get.sh localhost:13807 sampleKey "{\"10.10.0.5:13800\":2,\"10.10.0.6:13800\":0,\"10.10.0.7:13800\":0}"
